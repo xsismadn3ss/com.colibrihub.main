16aae6fb0910421ea13103b258b637e04c15f1b4
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-02-21 16:44:20 -0600

    Add endpoints to filter movies by producer and platform

--- src/main/java/com/colibrihub/main/controller/PeliculaController.java
+++ src/main/java/com/colibrihub/main/controller/PeliculaController.java
@@ -2,6 +2,7 @@ package com.colibrihub.main.controller;
 
 import com.colibrihub.main.dto.PeliculaDTO;
 import com.colibrihub.main.payload.RequestDTO;
+import com.colibrihub.main.payload.RequestStringDTO;
 import com.colibrihub.main.service.PeliculaService;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
@@ -21,20 +22,29 @@ public class PeliculaController {
     private static final Logger log = LoggerFactory.getLogger(PeliculaController.class);
     private PeliculaService peliculaService;
 
-    public PeliculaController(PeliculaService peliculaService){
+    public PeliculaController(PeliculaService peliculaService) {
         this.peliculaService = peliculaService;
     }
 
     @PostMapping("/obtenerPelicula")
-    public ResponseEntity<PeliculaDTO> obtenerPelicula(@RequestBody RequestDTO requestDTO){
+    public ResponseEntity<PeliculaDTO> obtenerPelicula(@RequestBody RequestDTO requestDTO) {
         log.info("La petición ha llegado al controlador");
         log.info("El parametro recibido es {}:", requestDTO);
-        return new ResponseEntity<>(peliculaService.obtenePelicula(requestDTO), HttpStatus.OK) ;
+        return new ResponseEntity<>(peliculaService.obtenePelicula(requestDTO), HttpStatus.OK);
     }
 
-    @PostMapping("/buscarPeliculas")
-    public  ResponseEntity<List<PeliculaDTO>> buscarPeliculas(@RequestBody RequestDTO requestDTO){
-        return  new ResponseEntity<>(peliculaService.buscarPeliculas(requestDTO), HttpStatus.OK);
+    @PostMapping("/pelicula/nombre")
+    public ResponseEntity<List<PeliculaDTO>> buscarPeliculas(@RequestBody RequestDTO requestDTO) {
+        return new ResponseEntity<>(peliculaService.buscarPeliculas(requestDTO), HttpStatus.OK);
     }
 
+    @PostMapping("/pelicula/productora")
+    public ResponseEntity<List<PeliculaDTO>> filtrarPorProductora(@RequestBody RequestStringDTO requestStringDTO) {
+        return new ResponseEntity<>(peliculaService.filtrarPeliculasProductora(requestStringDTO), HttpStatus.OK);
+    }
+
+    @PostMapping("/pelicula/plataforma")
+    public ResponseEntity<List<PeliculaDTO>> filtrarPorPlataforma(@RequestBody RequestStringDTO requestStringDTO) {
+        return new ResponseEntity<>(peliculaService.filtrarPeliculasPlataforma(requestStringDTO), HttpStatus.OK);
+    }
 }
