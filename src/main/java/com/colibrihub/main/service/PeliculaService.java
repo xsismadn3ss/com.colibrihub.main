d4ec191ea582f57d7acc7022ac249e62defc9fd1
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-02-21 16:44:44 -0600

    Add filtering methods to PeliculaService for producer and platform

--- src/main/java/com/colibrihub/main/service/PeliculaService.java
+++ src/main/java/com/colibrihub/main/service/PeliculaService.java
@@ -2,11 +2,18 @@ package com.colibrihub.main.service;
 
 import com.colibrihub.main.dto.PeliculaDTO;
 import com.colibrihub.main.payload.RequestDTO;
+import com.colibrihub.main.payload.RequestStringDTO;
 
 import java.util.List;
 
 public interface PeliculaService {
 
     PeliculaDTO obtenePelicula(RequestDTO requestDTO);
+
     List<PeliculaDTO> buscarPeliculas(RequestDTO requestDTO);
+
+    List<PeliculaDTO> filtrarPeliculasProductora(RequestStringDTO requestStringDTO);
+
+    List<PeliculaDTO> filtrarPeliculasPlataforma(RequestStringDTO requestStringDTO);
+
 }
